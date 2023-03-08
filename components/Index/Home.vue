bbad6fdab916f76c636330f94b97d69f10f82845
Momoleng <mouyleng@Momolengs-MacBook-Pro.local>
2023-03-08 08:54:08 +0700

    home

--- components/Index/Home.vue
+++ components/Index/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="lg:grid lg:grid-cols-2 h-screen lg:bg-none bg-primary">
     <div class="
-                                bg-primary
-                                flex flex-col
-                                justify-center
-                                items-start
-                                lg:px-20
-                                md:px-16
-                                px-6
-                                lg:py-16
-                                pt-44
-                                text-white
-                              ">
+                                  bg-primary
+                                  flex flex-col
+                                  justify-center
+                                  items-start
+                                  lg:px-20
+                                  md:px-16
+                                  px-6
+                                  lg:py-16
+                                  pt-44
+                                  text-white
+                                ">
       <div class="uppercase text-sm flex" data-aos="fade-down" data-aos-duration="1900">
         <svg class="h-5 w-5 transform rotate-45 text-blue-500" viewBox="0 0 24 24">
           <path fill="currentColor" d="M1,21H23L12,2" />
@@ -32,7 +32,7 @@
       </div>
       <div class="pt-4" data-aos="fade-up-left" data-aos-duration="2800">
         <button class="btn w-44 flex items-center justify-center bg-blue-500 py-3
-                                    px-6 lg:text-sm text-xs font-medium">
+                                      px-6 lg:text-sm text-xs font-medium">
           <div>Get Started</div>
           <svg class="h-4 w-4 svg-icon" viewBox="0 0 24 24">
             <path fill="currentColor" d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
@@ -50,26 +50,4 @@
 export default {};
 </script>
 
-<style>
-.svg-icon {
-  margin: 0 0 0 10px;
-}
-
-.btn:hover .svg-icon {
-  margin: 0 0 0 20px;
-  transition: 0.3s;
-}
-
-.btn:not(:hover) .svg-icon {
-  transition: 0.3s;
-}
-
-.btn:active {
-  transform: scale(0.9, 0.9);
-  transition: 200ms;
-}
-
-.btn:not(:active) {
-  transition: 200ms;
-}
-</style>
+<style></style>
